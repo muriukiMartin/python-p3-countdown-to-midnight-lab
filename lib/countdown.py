37b270df35a74b87d93a3229fdb3f8885789896b
Martin Muriuki <helheim254@gmail.com>
2023-08-23 22:14:00 +0300

    Completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
 # your code goes here!
+def countdown(count):
+    for i in range(count, 0, -1):                      #count is the starting number, 
+        print(f'{i} SECOND(S)!')                       #0 is the end of the range, itself not included
+    print("HAPPY NEW YEAR!")                           #-1 is the decrement after each iteration
+
+
+def countdown_with_sleep(count):                            
+    for i in range(count, 0, -1):                      
+        print(f'{i} SECOND(S)!')                       
+    print("HAPPY NEW YEAR!")
